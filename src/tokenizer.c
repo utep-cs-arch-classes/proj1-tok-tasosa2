ee443d9891af06b7b9da77124444750b75e86061
Thomas Sosa <tasosa2@miners.utep.edu>
2021-02-11 22:41:32 -0700

    Finished the first milestone. Taking and priting user input.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,2 +1,19 @@
-// comment for first commit
+#include <stdio.h>
+
+void main(){
+  
+  //Print out indicator
+  printf("> ");
+  
+  char c;
+
+  //Reads one char at a time from user input and prints them.
+  //This goes on until you press enter and create a new line.
+  while ((c = getchar()) != '\n'){
+    putchar(c);
+  }
+
+  printf("\n");
+  
+}
 
