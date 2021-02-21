08ceeecb66fcac6d3c71743b0e66246bf5fc924c
Thomas Sosa <tasosa2@miners.utep.edu>
2021-02-21 15:50:46 -0700

    Finished the first two functions, space_char and non_space_char. I changed the name of whatever word_end function was before to word_end. I am working with someone in the other class so I got mixed up with what he named his function there. I also created a main function in tokenizer.c to quickly test if the other functions work.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 
+int main(){
+  
+  //space_char tests
+  printf("%d", space_char('a'));// 0
+  printf("\n%d", space_char(' ')); // 1
+
+  //non_space_char tests
+  printf("\n%d", non_space_char('a'));// 1
+  printf("\n%d", non_space_char(' ')); // 0
+
+  return 0;
+}
 
 int space_char(char c){
-  
+  // True if c is a whitespace character and not a zero
+  if((c == '\t' || c == ' ' || c == '\n') && c != '\0'){
+    return 1;
+  }
+    return 0;
 }
 
 int non_space_char(char c){
+  // True if c is a non-whitespace character and not a zero
+  if((c != '\t' || c != ' ' || c != '\n') && c != '\0'){
+    return 1;
+  }
+    return 0;
+}
+
+/*char *word_start(char *str){
 
 }
 
-int word_start(char *str){
-
-}
-
-char *word_terminator(char *word){
-  
-}
-
-char *word_terminator(char *word){
+char *word_end(char *word){
   
 }
 
@@ -26,4 +43,5 @@ int count_words(char *str){
 
 }
 
+*/
 
